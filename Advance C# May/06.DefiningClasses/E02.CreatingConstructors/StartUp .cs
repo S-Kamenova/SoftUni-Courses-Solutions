b7335b87d4a2ca6f4a27a3f53382fc060f7192e6
S-Kamenova <suzi7_kamenova@abv.bg>
2022-06-04 10:38:34 +0300

    upload solutions from 06.DefiningClasses

---
+++ Advance C# May/06.DefiningClasses/E02.CreatingConstructors/StartUp .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
