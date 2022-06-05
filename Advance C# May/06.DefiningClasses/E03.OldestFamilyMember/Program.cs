eb5cee4e818e29208b58c0cf012ba6956a8f9eb2
S-Kamenova <suzi7_kamenova@abv.bg>
2022-06-05 20:14:49 +0300

    upload task 03 from DefiningClasses

---
+++ Advance C# May/06.DefiningClasses/E03.OldestFamilyMember/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Person person = new Person(input.Split()[0], int.Parse(input.Split()[1]));
+                family.AddMember(person);
+            }
+
+            Person oldest = family.GetoldestMember();
+            Console.WriteLine(oldest.Name + " " + oldest.Age);
+
+        }
+    }
+}
