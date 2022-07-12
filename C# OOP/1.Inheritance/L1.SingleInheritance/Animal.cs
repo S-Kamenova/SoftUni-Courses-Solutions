12b66d3ffe5c0f5c79bdc3e1c990ecbb1d71b235
S-Kamenova <suzi7_kamenova@abv.bg>
2022-07-12 21:03:13 +0300

    add task from OOP

---
+++ C# OOP/1.Inheritance/L1.SingleInheritance/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Animal
+    {
+        public static void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
