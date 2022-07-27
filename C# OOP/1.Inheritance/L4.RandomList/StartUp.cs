9dc72065422a6643b7f24f961c484d97943bfe97
S-Kamenova <suzi7_kamenova@abv.bg>
2022-07-27 19:28:34 +0300

    add  task

---
+++ C# OOP/1.Inheritance/L4.RandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("pesho"); 
+            list.Add("pesho1");
+            list.Add("pesho2");
+            list.Add("pesho3");
+
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
